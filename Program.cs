25afceb6243789ab13f0f263251fde1b9a3ad4c2
Alok Sharma <56442060+alok744@users.noreply.github.com>
2023-09-11 00:36:55 +0530

    Interface inheritance

--- Program.cs
+++ Program.cs
@@ -4,26 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace @interface
+namespace interface_inheritance
 {
-    interface IEmployee
-    {
-        void show();
-    }
-    class partimeEmployees : IEmployee
-    {
-        public void show()
-        {
-            Console.WriteLine("this is a method on IEmployee interface");
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            partimeEmployees ptr=new partimeEmployees();
-            ptr.show();
-            Console.ReadLine();
         }
     }
 }

6442d19097d9abeb03664bdff7a1ae5f0b13af2e
Alok Sharma <56442060+alok744@users.noreply.github.com>
2023-09-11 11:27:39 +0530

    prime number program

--- Program.cs
+++ Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace interface_inheritance
+namespace prime_number
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int count = 0;
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("Number is prime");
+            }
+            else
+            {
+                Console.WriteLine("it is not a prime number");
+            }
+            Console.ReadLine();
+
         }
+
     }
-}

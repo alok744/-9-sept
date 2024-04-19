50c01382fd89d171bdf84df35d11070467001ff5
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-04-19 16:39:10 +0530

    Reverse_string

--- Program.cs
+++ Program.cs
@@ -2,34 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace interface1
+namespace reverse_string1
 {
-    internal class Program
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //string name = "hello world";
+    //        //char[] reverse=name.ToCharArray();
+    //        //Array.Reverse(reverse);
+    //        //string revesename=new string(reverse);
+    //        //Console.WriteLine(revesename);
+    //        //Console.ReadLine();
+    //        string name = "Hello Alok";
+    //        char[] reverse = name.ToCharArray();
+    //        Array.Reverse(reverse);
+    //        string reversename = new string(reverse);
+    //        Console.WriteLine(reversename);
+    //        Console.ReadLine();
+    //    }
+    //         //Console.Write("Enter a string: ");
+    //        //string input = Console.ReadLine();
+
+    //        //string reversed = ReverseString(input);
+
+    //        //Console.WriteLine("Reversed string: " + reversed);
+    //        //Console.ReadLine();
+    //    //}
+
+    //    //static string ReverseString(string str)
+    //    //{
+    //    //    char[] charArray = str.ToCharArray();
+    //    //    Array.Reverse(charArray);
+    //    //    return new string(charArray);
+    //    //    Console.ReadLine();
+    //    //}
+        
+    //}
+    public class reversestring
     {
-        interface interface1
-        {
-            //method having only declaration
-            //not defination
-            void show();
-        }
-        class myclass:interface1
-        {
-            public void show()
-            {
-                Console.WriteLine("welcome to c sharp world!!!");
-            }
-            
-        }
-        // main method
         static void Main(string[] args)
         {
-            //creating obj
-            myclass obj1=new myclass();
-            //cretating method
-            obj1.show();
+       
+            string name = "helloworld";
+            string reverse = "";
+            for(int i= name.Length-1;i>=0; i--)
+            {
+                reverse = reverse + name[i];
+            }
+            Console.WriteLine(reverse);
             Console.ReadLine();
+            //char[] reversest=name.ToCharArray();
+            //Array.Reverse(reversest);
+            //Console.WriteLine(reversest);
+            //Console.ReadKey();
         }
+
+
     }
 }
